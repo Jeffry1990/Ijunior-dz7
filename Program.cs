728c8cebd95073bf865745d81d52b9595a50cee3
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2024-12-08 00:22:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp46
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userMessage;
+            int numbersOfRepeat;
+
+            Console.Write("Напишите любое слово: ");
+            userMessage = Console.ReadLine();
+            Console.Write("Сколько раз его вывести на экран?: ");
+            numbersOfRepeat = Convert.ToInt32(Console.ReadLine());
+
+            while (numbersOfRepeat > 0)
+            {
+                Console.WriteLine(userMessage);
+                numbersOfRepeat--;
+            }
+        }
+    }
+}
